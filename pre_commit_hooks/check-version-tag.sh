bbbf305f4840c658db28b60f1bde7a4d4e5379d8
Joseph P. <joseph.pena.q@gmail.com>
2025-03-26 10:03:30 -0300

    feat: check tag of the pyproject in tag

--- pre_commit_hooks/check-version-tag.sh
+++ pre_commit_hooks/check-version-tag.sh
@@ -5,15 +5,15 @@ set -e
 LATEST_TAG=$(git describe --tags --abbrev=0 2>/dev/null || echo "")
 
 # Get the version from pyproject.toml
-PYPROJECT_VERSION=$(grep -Po '(?<=^version = ")[^"]+' pyproject.toml)
+PYPROJECT_VERSION=$(git show "$LATEST_TAG:pyproject.toml" | grep -Po '(?<=^version = ")[^"]+')
 
 if [[ -z "$LATEST_TAG" ]]; then
-    echo "No existing tag found. Proceeding..."
+    echo "No existing tag found. Proceeding with the current version from pyproject.toml ($PYPROJECT_VERSION)..."
     exit 0
 fi
 
 if [[ "$LATEST_TAG" != "v$PYPROJECT_VERSION" && "$LATEST_TAG" != "$PYPROJECT_VERSION" ]]; then
-    echo "ERROR: The latest git tag ($LATEST_TAG) does not match the version in pyproject.toml (v$PYPROJECT_VERSION or $PYPROJECT_VERSION)."
+    echo "ERROR: The latest git tag ($LATEST_TAG) does not match the version in pyproject.toml ($PYPROJECT_VERSION)."
     exit 1
 fi
 
